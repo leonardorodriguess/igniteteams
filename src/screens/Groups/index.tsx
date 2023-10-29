bdc0d3053fde33792c5fb60e980f0175bdd6bfea
Leonardo <leonardorodrigues@grendene.com.br>
2023-10-29 18:18:17 -0300

    feat: using theme

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,8 @@
-import { StyleSheet, Text, View } from "react-native";
 import { Container, Title } from "./styles";
 
 export function Groups() {
   return (
     <Container>
-      <Title>Groups</Title>
     </Container>
   );
 }
