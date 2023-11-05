db4b4d5247d36dc2ca55cd730517419653ff77da
Leonardo <leo-feitosa@live.com>
2023-11-05 16:02:45 -0300

    style: add status bar style

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import { Groups } from '@screens/Groups';
 import theme from '@theme/index';
-import { ActivityIndicator } from 'react-native';
+import { ActivityIndicator, StatusBar } from 'react-native';
 import { Loading } from '@components/Loading';
 import { ThemeProvider } from 'styled-components';
 
@@ -10,6 +10,11 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
       {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
