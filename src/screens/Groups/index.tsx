409eb74ff1a6b58ef6ab107724324ea8f5d80460
Leonardo <leo-feitosa@live.com>
2023-11-16 10:12:59 -0300

    feat: add component button

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -5,6 +5,7 @@ import { GroupCard } from "@components/GroupCard";
 import { useState } from "react";
 import { FlatList } from "react-native";
 import { ListEmpty } from "@components/ListEmpty";
+import { Button } from "@components/Button";
 
 export function Groups() {
   const [groups, setGroups] = useState<string[]>(["Galera da Rocket"]);
@@ -16,14 +17,16 @@ export function Groups() {
       <Highlight title="Turmas" subtitle="jogue com a sua turma" />
 
       <FlatList
-        data={[]}
+        data={groups}
         keyExtractor={(item) => item}
         renderItem={({ item }) => <GroupCard title={item} />}
-        contentContainerStyle={{ flexGrow: 1}}
+        contentContainerStyle={{ flexGrow: 1 }}
         ListEmptyComponent={ListEmpty({
           message: "Que tal cadastrar  a primeira turma?",
         })}
       />
+
+      <Button title="Criar nova turma"/>
     </Container>
   );
 }
