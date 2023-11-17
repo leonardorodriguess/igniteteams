ce1eee7d5af878be90b5788bc85a87cc2b0983e1
Leonardo <leo-feitosa@live.com>
2023-11-17 08:43:21 -0300

    feat: add input custom

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -2,6 +2,7 @@ import { Header } from "@components/Header";
 import { Container, Content, Icon } from "./styles";
 import { Highlight } from "@components/Highlight";
 import { Button } from "@components/Button";
+import { Input } from "@components/Input";
 
 export function NewGroup() {
   return (
@@ -15,6 +16,8 @@ export function NewGroup() {
           title="Nova turma"
           subtitle="crie a turma para adicionar as pessoas"
         />
+
+        <Input />
 
       <Button title="Criar" />
       </Content>
