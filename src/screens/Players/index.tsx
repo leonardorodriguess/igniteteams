97cfeac5f176481ef8b9ab185e75aa6accb7903c
Leonardo <leo-feitosa@live.com>
2023-11-19 14:56:08 -0300

    refact: reviw name

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -7,7 +7,7 @@ import { Highlight } from "@components/Highlight";
 import { ListEmpty } from "@components/ListEmpty";
 import { ButtonIcon } from "@components/ButtonIcon";
 import { PlayerCard } from "@components/PlayerCard";
-import { Container, Form, HeaderList, NumbersOfPlayers } from "./styles";
+import { Container, Form, HeaderList, NumberOfPlayers } from "./styles";
 import { Button } from "@components/Button";
 
 export function Players() {
@@ -42,7 +42,7 @@ export function Players() {
           )}
           horizontal
         />
-        <NumbersOfPlayers> {players.length} </NumbersOfPlayers>
+        <NumberOfPlayers> {players.length} </NumberOfPlayers>
       </HeaderList>
 
       <FlatList
