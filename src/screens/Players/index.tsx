1e30e38eaa4ff9900fe0f780802dad153cc877c2
Leonardo <leo-feitosa@live.com>
2023-11-19 13:48:02 -0300

    feat: add filter

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -3,6 +3,7 @@ import { Container, Form } from "./styles";
 import { Highlight } from "@components/Highlight";
 import { ButtonIcon } from "@components/ButtonIcon";
 import { Input } from "@components/Input";
+import { Filter } from "@components/Filter";
 
 export function Players() {
   return (
@@ -23,6 +24,10 @@ export function Players() {
 
         <ButtonIcon icon="add" />
       </Form>
+
+      <Filter 
+        title="Time A"
+      />
 
     </Container>
   );
